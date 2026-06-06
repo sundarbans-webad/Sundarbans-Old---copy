505d6de4e876be4ce1d97077ed1d5a06909160d3
Sundarbans Webadmin <sundarbans-webad@ds.study.iitm.ac.in>
2026-06-06 10:15:14 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import './index.css'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+    userDetails: null,
+  }
+  },
+  mounted() {
+    const storedUserDetails = localStorage.getItem('userDetails');
+    if (storedUserDetails) {
+      const userDetails = JSON.parse(storedUserDetails);
+      this.userDetails = userDetails;
+      // console.log("App.vue : ", userDetails)
+    }},
+}
+</script>
+
+<style>
+</style>
